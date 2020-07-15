86d97965d28253faaa5be5d3b4f4a723f4293ee4
sumanth <sumanthhegde1029@gmail.com>
2020-07-15 11:01:06 +0530

    flatlist

--- App.js
+++ App.js
@@ -1,38 +1,55 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("hegde");
-
-  
+  const [people, setPeople] = useState([
+    { name: "sumanth", id: "1" },
+    { name: "hegde", id: "2" },
+    { name: "shreedhar", id: "3" },
+    { name: "madhura", id: "4" },
+    { name: "sumu", id: "5" },
+    { name: "sumu", id: "6" },
+    { name: "sumu", id: "7" },
+    { name: "sumu", id: "8" },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <TextInput 
-      style={styles.buttonc}
-      placeholder="Enter name" 
-      onChangeText={(val)=>setName(val)}/>
+      {/* <ScrollView>
+        {people.map((p) => {
+          return (
+            <View key={p.key}>
+              <Text style={styles.item}>{p.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView> */}
 
-      <Text>Enter name</Text>
+      <FlatList
+      numColumns={2}
+      keyExtractor={(item)=>item.id}
+        data={people}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
     </View>
   );
 }
-
+//df
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "black",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  buttonc: {
-    marginTop: 20,
-    borderWidth: 1,
-    borderColor: "black",
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item: {
+    marginTop: 24,
+    padding: 30,
+    borderColor: "#222",
+    borderWidth: 2,
+    color: "white",marginHorizontal:10,marginTop:24
   },
 });
